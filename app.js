caf9192a27c3e4d89b0e8800f7e87874b24a36d1
kongnuy <kongnuyvictorien@yahoo.com>
2020-04-03 22:46:22 +0100

    Second commit

--- app.js
+++ app.js
@@ -1,39 +1,60 @@
-// Copyright 2017 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 'use strict';
 
-// [START gae_node_request_example]
 const express = require('express');
-
 const app = express();
 const http = require('http').createServer(app)
-const socketio = require('socket.io')(http)
-var cors = require('cors')
-app.use(cors());
+
+const socketio = require('socket.io')(http,  {
+    handlePreflightRequest: (req, res) => {
+        const headers = {
+            "Access-Control-Allow-Headers": "Content-Type, Authorization",
+            "Access-Control-Allow-Origin": req.headers.origin, //or the specific origin you want to give access to,
+            "Access-Control-Allow-Credentials": true
+        };
+        res.writeHead(200, headers);
+        res.end();
+    }
+});
+
+//var cors = require('cors')
+//app.use(cors());
+app.use(function (req, res, next) {
+  console.log('Time:', Date.now());
+  console.log(req.originalUrl);
+  next();
+});
 
 app.get('/', (req, res) => {
   res.status(200).send('Node Server is running. Yay!! \n <a href="/chat">Aller au chat</a>').end();
 });
+
 app.get('/chat', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+io.use((socket, next) => {
+  let handshake = socket.handshake;
+  console.log("----- Socket info -----");
+  console(handshake.url)
+  console(handshake.address)
+  console(handshake.headers)
+  console.log("----- end of Socket info -----");
+
+});
+
 socketio.on("connection", (userSocket) => {
     userSocket.on('chat message', function(msg){
-     console.log('message: ' + msg);
-     userSocket.broadcast.emit('chat message', msg)
+      if (typeof msg === "object") {
+        try {
+          console.log('message: ' + msg.message);
+          userSocket.broadcast.emit('chat message', msg.message)
+        } catch (error) {
+          console.log(error)
+        }
+      } else {
+        console.log('message: ' + msg);
+        userSocket.broadcast.emit('chat message', msg)
+      }
     });
     userSocket.on("send_message", (data) => {
         console.log(JSON.stringify(data))
