d709b7760df14b8fca18212bc5a32e13f003d741
kongnuy <kongnuyvictorien@gmail.com>
2020-04-03 13:55:54 +0100

    Update message format

--- app.js
+++ app.js
@@ -1,20 +1,5 @@
-// Copyright 2017 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 'use strict';
 
-// [START gae_node_request_example]
 const express = require('express');
 
 const app = express();
@@ -32,8 +17,17 @@ app.get('/chat', function(req, res){
 
 socketio.on("connection", (userSocket) => {
     userSocket.on('chat message', function(msg){
-     console.log('message: ' + msg);
-     userSocket.broadcast.emit('chat message', msg)
+      if (typeof msg === "object") {
+        try {
+          console.log('message: ' + msg.message);
+          userSocket.broadcast.emit('chat message', msg.message)
+        } catch (error) {
+          console.log(error)
+        }
+      } else {
+        console.log('message: ' + msg);
+        userSocket.broadcast.emit('chat message', msg)
+      }
     });
     userSocket.on("send_message", (data) => {
         console.log(JSON.stringify(data))
@@ -41,12 +35,10 @@ socketio.on("connection", (userSocket) => {
     })
 })
 
-// Start the server
 const PORT = process.env.PORT || 8080;
 http.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
   console.log('Press Ctrl+C to quit.');
 });
-// [END gae_node_request_example]
 
 module.exports = app;
